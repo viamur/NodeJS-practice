1a9b987f0de2a9d3f2885621b398b1be945b4cd8
Moskalenko Serhii <mynameserhii@gmail.com>
2022-09-11 19:17:50 +0300

    d

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const fs = require('fs').promises;
+const path = require('path');
 
+const bookPath = path.join(__dirname)
+
+async function getAll() {
+    const result = await fs.readFile(bookPath);
+
+    return JSON.parse(result.toString());
+}
+
+async function addBook(title, author) 
+const books = await getAll();
+const book = books.find(({author}) => author === author)
+
+if (!book) {
+    throw new Error('book not found');
+} 
 // fs.readdir(__dirname)
 //   .then(files => {
 //     return Promise.all(
@@ -19,3 +35,6 @@ const fs = require('fs').promises;
 // fs.rename('test.txt', 'hello.txt');
 // fs.readFile('hello.txt').then(data => console.log(data.toString()));
 // fs.unlink('test.txt');
+
+fs.readFile('hello.txt', 'utf-8').then(data => console.log(data));
+

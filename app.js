c4f474ff0ff3cd46f65b608c2782ba630b8d94de
Moskalenko Serhii <mynameserhii@gmail.com>
2022-09-11 22:28:53 +0300

    w

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
-const fs = require('fs').promises;
-const path = require('path');
+const http = require('http');
+const port = 3000;
 
-const bookPath = path.join(__dirname)
+const server = http.createServer((req, res) => {
+  res.end();
+});
 
-async function getAll() {
-    const result = await fs.readFile(bookPath);
-
-    return JSON.parse(result.toString());
-}
-
-async function addBook(title, author) 
-const books = await getAll();
-const book = books.find(({author}) => author === author)
-
-if (!book) {
-    throw new Error('book not found');
-} 
-// fs.readdir(__dirname)
-//   .then(files => {
-//     return Promise.all(
-//       files.map(async filename => {
-//         const stats = await fs.stat(filename);
-//         return {
-//           Name: filename,
-//           Size: stats.size,
-//           Date: stats.mtime,
-//         };
-//       })
-//     );
-//   })
-//   .then(result => console.log(result));
-
-// fs.appendFile('test.txt', 'data in txt files');
-// fs.rename('test.txt', 'hello.txt');
-// fs.readFile('hello.txt').then(data => console.log(data.toString()));
-// fs.unlink('test.txt');
-
-fs.readFile('hello.txt', 'utf-8').then(data => console.log(data));
-
+server.listen(port, () => console.log('Server listening on port ' + port));

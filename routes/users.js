11b00173d1aba143b7552fcf00df5f39cec0c1cf
Moskalenko Serhii <mynameserhii@gmail.com>
2022-09-21 21:54:14 +0300

    remove "contacts'

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const contacts = [
-  { id: '1', username: 'Felix', surname: 'Brown', email: 'felix@test.com' },
-  { id: '2', username: 'Sonya', surname: 'Redhead', email: 'sonya@test.com' },
-  { id: '3', username: 'Conan', surname: 'Barbarian', email: 'conan@test.com' },
-];
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.json(contacts);

0a3cb6f8cf4f1f227fb2284435752bd52c9abd30
Moskalenko Serhii <mynameserhii@gmail.com>
2022-09-22 22:45:53 +0300

    done

--- app.js
+++ app.js
@@ -1,83 +1,42 @@
 const express = require('express');
-const path = require('path');
+const { connect } = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const users = require('./routes/users');
+require('dotenv').config();
+
 const app = express();
-const { connectMongo, getCats } = require('./db/connection');
 
-const myRouter = require('./myRoute');
+/* TAKE CONST FROM .ENV */
+const PORT = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:3000';
 
-// app.get('/contact*', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-app.use((req, res, next) => {
-  const data = getCats();
-  req.db = { ...data };
-  next();
-});
 app.use(express.json());
-app.use('/', myRouter);
-// app.use((req, res, next) => {
-//   console.log('Наше промежуточное ПО');
-//   //   next();
-// });
+app.use(morgan('dev'));
+app.use(cors());
 
-// app.use(express.static(path.join(__dirname + '/public')));
+app.use('/', users);
 
-// app.get('/constacts/:id', (req, res) => {
-//   res.send(`<h1>${req.params.id}</h1>`);
-// });
+app.use(async function (req, res) {
+  res.status(404).send('<h1>Page Not Found</h1>');
+});
 
-// app.get('/contacts', (req, res) => {
-//     console.log(req.query)
-//   res.send(`<h1>${req.query}</h1>`);
-// });
+app.use(function (err, req, res, next) {
+  const { status = 500, message = 'Internal server error' } = err;
+  console.error(err);
+  res.status(status).json({ message });
+});
 
-// app.use(express.json());
+/* CONNECT TO MONGO DB */
+connect(mongoURL)
+  .then(() => {
+    console.log('MongoDB connected');
 
-// app.post('/login', (req, res, next) => {
-//   console.log(req.body);
-//   res.status(201);
-//   res.send('Ok');
-// });
-
-// app
-//   .route('/blog')
-//   .get((req, res) => {
-//     res.send('this get');
-//   })
-//   .post((req, res) => {
-//     res.send('this post');
-//   })
-//   .delete((req, res) => {
-//     res.send('this delete');
-//   });
-
-// app.get('/user/:id/phone', (req, res) => {
-//   console.log(req.params);
-//   res.send(`<h1>${req.params.id}</h1>`);
-// });
-
-/* СКАЧИВАНИЕ ФАЙЛОВ */
-// app.get('/download', (req, res) => {
-//   const file = __dirname + '/public/test.txt';
-//   res.download(file, 'hello.html');
-// });
-
-// app.get('/download', (req, res) => {
-//   const file = __dirname + '/public/index.html';
-//   res.sendFile(file);
-// });
-
-// app.get('/download', (req, res) => {
-//   res.redirect('https://google.com/');
-// });
-
-const start = async () => {
-  await connectMongo();
-
-  app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
   });
-};
-
-start();
---
+++ service/schemas/contact.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const contact = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    who: {
+      type: String,
+      enum: ['admin', 'staff', 'developer', 'user'],
+      default: 'user',
+    },
+    kod: {
+      type: String,
+      match: /[0-1]{1}-[0-9]{2}/,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Contact = model('Contacts', contact);
+
+module.exports = Contact;

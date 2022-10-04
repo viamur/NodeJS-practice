e5162333664d4a8ef2ccf82b0735ac8ecd871f62
Moskalenko Serhii <mynameserhii@gmail.com>
2022-10-04 16:57:10 +0300

    done

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const service = require('../service');
+const auth = require('../service/authUser');
 
 const router = express.Router();
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
+router.get('/', auth, async (req, res, next) => {
+  const { id } = req.user;
   try {
-    const data = await service.getAllContacts();
+    const data = await service.getAllContacts({ id });
     res.json(data);
   } catch (error) {
     res.status(404).send(error.message);
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', auth, async (req, res, next) => {
+  const { id } = req.user;
   try {
-    const data = await service.createContact(req.body);
+    const data = await service.createContact({ id, data: req.body });
     res.json(data);
   } catch (error) {
     res.status(404).send(error.message);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
   try {
-    const result = await service.deleteContact(req.params.id);
+    const result = await service.deleteContact({ id: req.params.id, owner: req.user.id });
     res.status(200).json(result);
   } catch (error) {
     res.status(404).send(error.message);
--- service/index.js
+++ service/index.js
@@ -1,15 +1,37 @@
 const Contact = require('./schemas/contact');
+const User = require('./schemas/user');
 
-const getAllContacts = async () => {
-  return Contact.find();
+const getAllContacts = async ({ id }) => {
+  return Contact.find({ owner: id }).populate('owner', 'name');
 };
 
-const createContact = async body => {
-  return Contact.create(body);
+const createContact = async ({ id, data }) => {
+  return Contact.create({ owner: id, email: data.email, name: data.name });
 };
 
-const deleteContact = async id => {
-  return Contact.findByIdAndDelete(id);
+const deleteContact = async ({ id, owner }) => {
+  return Contact.findOneAndDelete({ id, owner });
 };
 
-module.exports = { getAllContacts, createContact, deleteContact };
+/* =============================USER=========================== */
+
+const checkUser = async ({ name }) => {
+  return User.findOne({ name: name });
+};
+
+const findByIdUser = async ({ id }) => {
+  return User.findById(id);
+};
+
+const signupUser = async ({ name, password }) => {
+  return User.create({ name, password });
+};
+
+module.exports = {
+  getAllContacts,
+  createContact,
+  deleteContact,
+  checkUser,
+  findByIdUser,
+  signupUser,
+};
